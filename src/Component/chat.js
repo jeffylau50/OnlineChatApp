8e83ea0f613199fdf1e2709d8047c2b7dc1a6c8a
Jeffy Lau <100321067+jeffy2560@users.noreply.github.com>
2022-04-12 17:04:49 -0700

    refactor

--- src/Component/chat.js
+++ src/Component/chat.js
@@ -1,40 +1,58 @@
-import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { auth, firestore } from '../firebase'
-import { useAuth } from '../context/AuthContext.js'
-import Message from './message.js'
-import './chat.css'
-import {useCollectionData} from 'react-firebase-hooks/firestore';
-
-
-
+import React, { useContext, useState } from "react";
+import firebase from 'firebase/compat/app';
+import { useNavigate } from "react-router-dom";
+import { auth, firestore } from "../firebase";
+import { useAuth } from "../context/AuthContext.js";
+import Message from "./message.js";
+import "./chat.css";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 function Chat() {
+  const messageRef = firestore.collection("messages");
+  const query = messageRef.orderBy("createdAt").limit(25);
+  const [messages] = useCollectionData(query, { idField: "id" });
 
-    const messageRef = firestore.collection('messages')
-    const query = messageRef.orderBy('createdAt').limit(25);
-    const [messages] = useCollectionData(query, {idField: 'id'});
+  const [formValue, setFormValue] = useState("");
 
-    const Navigate = useNavigate();
-    const { user } = useAuth();
-    const handleLogout = async () => {
-        await auth.signOut();
-        Navigate("/")
-    }
-    const handleClick = () => {
-    console.dir(messages)
-    }
+  const Navigate = useNavigate();
+  const { user } = useAuth();
+  const handleLogout = async () => {
+    await auth.signOut();
+    Navigate("/");
+  };
+  const handleChange = (evt) => {
+    setFormValue(evt.target.value);
+  };
 
-    return(
-<div className='chats-page'>
-   <div className='nav-bar'>
-       <div className='logo-tab'>🍊OrangeChat</div>
-       <div onClick={handleLogout} className='logout-tab'>Logout</div>
-       </div>
-{messages && messages.map(msg => <Message key={msg.idField} message={msg} />)}
-<button onClick={handleClick}>test</button>       
-</div>
-)
+  const handleSubmit = async (evt) => {
+  evt.preventDefault();
+  const {uid, photoURL} = auth.currentUser;
+  await messageRef.add({
+  text: formValue,
+  createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+  uid,
+  photoURL
+  })
+  setFormValue('')
+  }
+
+  return (
+    <div className="chats-page">
+      <div className="nav-bar">
+        <div className="logo-tab">🍊OrangeChat</div>
+        <div onClick={handleLogout} className="logout-tab">
+          Logout
+        </div>
+      </div>
+      {messages &&
+        messages.map((msg) => <Message key={msg.id} message={msg} />)}
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={formValue} onChange={handleChange} />
+        <button>Send</button>
+      </form>
+    </div>
+  );
 }
 
 export default Chat;
--- src/Component/message.js
+++ src/Component/message.js
@@ -9,9 +9,14 @@ class Message extends Component {
    
     render(){ 
         
-        const {text, uid} = this.props.message;
+        const {text, uid, photoURL} = this.props.message;
+        const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received' ;
+
         return(
+            <div>
+            <img src={photoURL} />
             <p>{text}</p>
+            </div>
         )
     }
 }
