80c74083a39746fc90a45d2a7c34cdba3729d4c8
Jeffy Lau <100321067+jeffy2560@users.noreply.github.com>
2022-04-12 20:18:52 -0700

    CSS style + time stamp + username function

--- src/Component/chat.js
+++ src/Component/chat.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import firebase from 'firebase/compat/app';
+import firebase from "firebase/compat/app";
 import { useNavigate } from "react-router-dom";
 import { auth, firestore } from "../firebase";
 import { useAuth } from "../context/AuthContext.js";
@@ -25,16 +25,18 @@ function Chat() {
   };
 
   const handleSubmit = async (evt) => {
-  evt.preventDefault();
-  const {uid, photoURL} = auth.currentUser;
-  await messageRef.add({
-  text: formValue,
-  createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-  uid,
-  photoURL
-  })
-  setFormValue('')
-  }
+    evt.preventDefault();
+    const { uid, photoURL } = await auth.currentUser;
+    const {displayName}  = await auth.currentUser.multiFactor.user
+    await messageRef.add({
+      text: formValue,
+      createdAt: new Date(),
+      uid,
+      photoURL,
+      displayName
+        });
+    setFormValue("");
+  };
 
   return (
     <div className="chats-page">
@@ -44,13 +46,131 @@ function Chat() {
           Logout
         </div>
       </div>
-      {messages &&
-        messages.map((msg) => <Message key={msg.id} message={msg} />)}
+      <body>
+        <div class="container-fluid h-100">
+          <div class="row justify-content-center h-100">
+            <div class="col-md-4 col-xl-3 chat">
+              <div class="card mb-sm-3 mb-md-0 contacts_card">
+                <div class="card-header">
+                  <div class="input-group">
+                    <input
+                      type="text"
+                      placeholder="Search..."
+                      name=""
+                      class="form-control search"
+                    />
+                    <div class="input-group-prepend">
+                      <span class="input-group-text search_btn">
+                        <i class="fas fa-search"></i>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-body contacts_body">
+                  <ui class="contacts">
+                    <li class="active">
+                      <div class="d-flex bd-highlight">
+                        <div class="img_cont">
+                          <img
+                            src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg"
+                            class="rounded-circle user_img"
+                          />
+                          <span class="online_icon"></span>
+                        </div>
+                        <div class="user_info">
+                          <span>Chat Room</span>
+                          <p>online</p>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div class="d-flex bd-highlight">
+                        <div class="img_cont">
+                          <img
+                            src="https://2.bp.blogspot.com/-8ytYF7cfPkQ/WkPe1-rtrcI/AAAAAAAAGqU/FGfTDVgkcIwmOTtjLka51vineFBExJuSACLcBGAs/s320/31.jpg"
+                            class="rounded-circle user_img"
+                          />
+                          <span class="online_icon offline"></span>
+                        </div>
+                        <div class="user_info">
+                          <span>Taherah Big</span>
+                          <p>Taherah left 7 mins ago</p>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div class="d-flex bd-highlight">
+                        <div class="img_cont">
+                          <img
+                            src="https://i.pinimg.com/originals/ac/b9/90/acb990190ca1ddbb9b20db303375bb58.jpg"
+                            class="rounded-circle user_img"
+                          />
+                          <span class="online_icon"></span>
+                        </div>
+                        <div class="user_info">
+                          <span>Sami Rafi</span>
+                          <p>Sami is online</p>
+                        </div>
+                      </div>
+                    </li>
+                  </ui>
+                </div>
+                <div class="card-footer"></div>
+              </div>
+            </div>
+            <div class="col-md-8 col-xl-6 chat">
+              <div class="card">
+                <div class="card-header msg_head">
+                  <div class="d-flex bd-highlight">
+                    <div class="img_cont">
+                      <img
+                        src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg"
+                        class="rounded-circle user_img"
+                      />
+                      <span class="online_icon"></span>
+                    </div>
+                    <div class="user_info">
+                      <span>Chat Room</span>
+                    </div>
+                  </div>
+                  <span id="action_menu_btn">
+                    <i class="fas fa-ellipsis-v"></i>
+                  </span>
+                </div>
+                <div class="card-body msg_card_body">
+                  {messages &&
+                    messages.map((msg) => (
+                      <Message key={msg.id} message={msg} />
+                    ))}
+                </div>
+                <div class="card-footer">
+                  <div class="input-group">
+                    <div class="input-group-append"></div>
+                    <form className="formCSS" onSubmit={handleSubmit}>
+                      <textarea
+                        className="form-control type_msg customInput1"
+                        placeholder="Type your message..."
+                        type="text"
+                        value={formValue}
+                        onChange={handleChange}
+                        minLength={7}
+                        required
+                      ></textarea>
+                      <div class="input-group-append">
+                        <button className="input-group-text send_btn customButton1">
+                          Send
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
 
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={formValue} onChange={handleChange} />
-        <button>Send</button>
-      </form>
+      
     </div>
   );
 }
--- src/Component/message.js
+++ src/Component/message.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
 import { auth } from '../firebase';
+import "./message.css";
 
 
 
-class Message extends Component {
+function Message(props) {
     
 
    
-    render(){ 
+    
         
-        const {text, uid, photoURL} = this.props.message;
-        const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received' ;
-
+        const {text, uid, photoURL, createdAt, displayName} = props.message;
+        
+        let newTime = createdAt.toDate().toString();
+        let gg = newTime.slice(15,21)
+        console.log(gg)
+        //const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received' ;
+        //let convertime = new Date(createdAt.seconds*1000)
+        //let finalTime = convertime.toString().slice(15,21)
+        //let finalDate = convertime.toString().slice(3,15)
+        
+        //console.log(displayName)
         return(
             <div>
-            <img src={photoURL} />
-            <p>{text}</p>
+
+            <div class="d-flex justify-content-start mb-4">
+								<div class="img_cont_msg">
+									<img src={photoURL} class="rounded-circle user_img_msg" />
+								</div>
+								<div class="msg_cotainer">
+                                    <b>{displayName}</b>
+                                    <br />
+									{text}
+									<span class="msg_time customTimestamp">{gg}</span>
+								</div>
+                                
+
+							</div>
+
             </div>
         )
     }
-}
+
 
 
 
